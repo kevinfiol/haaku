c82ca9b9369271e44d2d72cdae9fd6fe0afaf47b
kevinfiol <me@kevinfiol.com>
2022-12-14 11:28:05 -0500

    add autofreeze option

--- index.js
+++ index.js
@@ -1,24 +1,29 @@
+let AUTO_FREEZE = false,
+  isObj = x => x && Object.getPrototypeOf(x) === Object.prototype,
+  isArr = Array.isArray;
+
+let deepFreeze = obj => {
+  let dive = isObj(obj) || isArr(obj);
+  if (dive) for (let i in obj) deepFreeze(obj[i]);
+  return dive ? Object.freeze(obj) : obj;
+};
+
 let handler = {
   get(obj, k) {
-    let dive = false,
-      v = obj[k];
+    let v = obj[k],
+      dive = isObj(v) || isArr(v);
 
-    if (v && Object.getPrototypeOf(v) === Object.prototype) {
-      obj[k] = { ...v };
-      dive = true;
-    }
-    
-    if (Array.isArray(v)) {
-      obj[k] = [ ...v ];
-      dive = true;
-    }
+    if (isArr(v)) obj[k] = [ ...v ];
+    else if (dive) obj[k] = { ...v };
 
     return dive ? new Proxy(obj[k], handler) : v;
   }
 };
 
+export const setAutoFreeze = x => AUTO_FREEZE = x || false;
+
 export function from(obj, fn) {
   let n = { ...obj };
   fn(new Proxy(n, handler));
-  return n;
+  return AUTO_FREEZE ? deepFreeze(n) : n;
 }
