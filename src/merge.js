48e0da10270f9a9535e7219d81c50a1987614cec
kevinfiol <me@kevinfiol.com>
2025-05-27 13:02:46 -0400

    inline init logic

--- src/merge.js
+++ src/merge.js
@@ -1,23 +1,18 @@
-let isArray = Array.isArray,
-  isObj = x => x && Object.getPrototypeOf(x) === Object.prototype,
-  init = x => {
-    x = isArray(x) ? [ ...x ] : isObj(x) ? { ...x } : 0;
-    if (!x) throw Error('haaku: must merge array or object');
-    return x;
-  };
+let isObj = x => x && Object.getPrototypeOf(x) === Object.prototype;
 
-export function merge(obj, ...patches) {
-  obj = init(obj);
+export function merge(o, ...patches) {
+  o = Array.isArray(o) ? [ ...o ] : isObj(o) ? { ...o } : 0;
+  if (!o) throw Error('haaku: must merge array or object');
 
   for (let patch of patches) {
     for (let k in patch) {
       let v = patch[k];
-      if (typeof v === 'function') obj[k] = v(obj[k]);
-      else if (isObj(v) && isObj(obj[k])) obj[k] = merge(obj[k], v);
-      else if (v === void 0) delete obj[k];
-      else obj[k] = v;
+      if (typeof v === 'function') o[k] = v(o[k]);
+      else if (isObj(v) && isObj(o[k])) o[k] = merge(o[k], v);
+      else if (v === void 0) delete o[k];
+      else o[k] = v;
     }
   }
 
-  return obj;
+  return o;
 }
